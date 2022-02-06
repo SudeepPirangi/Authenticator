d6b06203e0b465d241f3fd40db2a540bb9337898
SudeepPirangi <sudeep_edu@yahoo.com>
2022-02-06 20:22:32 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT;
+
+const app:Express = express();
+
+app.use(express.json());
+app.use(helmet());
+
+app.listen(PORT, () => {
+  console.log(`Authenticator app is listening on port ${PORT} in ${ENV} environment`);
+});
